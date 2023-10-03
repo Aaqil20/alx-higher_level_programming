2ae1929a606ad86088350b7e90f389875479ecd2
Aaqil20 <135042498+Aaqil20@users.noreply.github.com>
2023-10-03 12:48:23 +0200

    Add files via upload

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
+for i in range(122, 96, -1):
+    if (i % 2 != 0):
+        print('{}'.format(chr(i - 32)), end='')
     else:
-        print('{:c}'.format(90 - i), end='')
+        print('{}'.format(chr(i)), end='')
